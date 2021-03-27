10ee317b324effb5c1977c6650303cee20c03c21
Yevgeny Levin <devlev1980@gmail.com>
2021-03-27 16:30:37 +0300

    add ng-container + ng-template to insert template to viewContainerRef

--- projects/advanced-template-techniques/src/app/app.component.ts
+++ projects/advanced-template-techniques/src/app/app.component.ts
@@ -8,9 +8,12 @@ import {
   Optional,
   Renderer2,
   SkipSelf,
+  TemplateRef,
   ViewChild,
+  ViewContainerRef,
   ViewRef,
 } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -20,9 +23,49 @@ import {
 export class AppComponent implements AfterViewInit {
   title = 'advanced-template-techniques';
 
-  constructor() {}
+  /**
+   * Set h2 with ngClass directive as a ViewContainer
+   */
+  @ViewChild(NgClass, { read: ViewContainerRef })
+  h2ContainerRef: ViewContainerRef;
 
-  ngAfterViewInit(): void {}
+  /**
+   * Set ng-container as a ViewContainer
+   */
+  @ViewChild('containerRef', { read: ViewContainerRef })
+  containerRef: ViewContainerRef;
 
-  onClick(): void {}
+  /**
+   * Use if there's one ng-template (no need reference in html like : "<ng-template #templateRef></ng-template");
+   */
+  @ViewChild(TemplateRef) template: TemplateRef<any>;
+
+  /**
+   * Use if there's more then one ng-template (need reference in html like : "<ng-template #templateRef></ng-template");
+   */
+  @ViewChild('templateRef', { read: TemplateRef })
+  templateRef: TemplateRef<any>;
+  /**
+   * Dependency Injection of ViewContainerRef and get App-Root as a ViewContainerRef
+   * @param viewContainer: ViewContainerRef
+   */
+  constructor(private viewContainer: ViewContainerRef) {}
+
+  ngAfterViewInit(): void {
+    console.log('View container', this.viewContainer);
+    const embeddedView: EmbeddedViewRef<any> = this.template.createEmbeddedView(
+      null
+    );
+    /**
+     * Insert embeddedView to app-root viewContainerRef
+     */
+    // this.viewContainer.insert(embeddedView);
+    /**
+     * Insert embeddedView to ng-container viewContainerRef
+     */
+    // this.containerRef.insert(embeddedView);
+
+    // Instead of using previous 2 line of code
+    this.containerRef.createEmbeddedView(this.template);
+  }
 }
